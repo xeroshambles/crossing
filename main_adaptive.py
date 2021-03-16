1a7809f40d4d08868522df824887c64cdbe62d24
zer0meister <nicholas.glorio@gmail.com>
2021-03-16 23:53:25 +0100

    fixed plots for adaptive mode

--- main_adaptive.py
+++ main_adaptive.py
@@ -232,7 +232,7 @@ if __name__ == "__main__":
 
 
 
-    linesPerMeasure(single_measures, labels, titles, colors, projects, projects_labels, numberOfVehicles, stepsSpawn,
+    linesPerMeasureAdaptive(single_measures, labels, titles, colors, projects, projects_labels, numberOfVehicles, stepsSpawn,
                     path)
 
 
